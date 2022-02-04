209be0970b18ff7f3ed5203db774d68c6537119c
bharathjinka09 <bharathjinka09@gmail.com>
2022-02-04 10:42:29 +0530

    Decorators example

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ arr = [1,3,4,6,7,8,10,13,14,18,19,21,24,37,40,45,71]
 elem = 7
 
 output = binary_search(arr, elem)
-print(output)
+# print(output)
 
 # test_list = [1,3,4,5,3,4]
 # print ("The original list is : " +  str(test_list))
@@ -88,30 +88,112 @@ print(output)
         
 #         for i in range(len(nums)):
 #             secondNumber = target-nums[i]
+#             print(secondNumber,"secondNumber")
 #             if(secondNumber in dictionary.keys()):
 #                 secondIndex = nums.index(secondNumber)
+#             	print(secondIndex,"secondIndex")
+#             	# print(dictionary,"dictionary")
+
 #                 if(i != secondIndex):
 #                     return sorted([i, secondIndex])
                 
 #             dictionary.update({nums[i]: i})
-
 # res = twoSum([1,2,3],3)
 # print(res)
 
 
 
 
+# def bi_search(arr,elem):
+# 	low = 0
+# 	high = len(arr)-1
+
+# 	while low <= high:
+# 		middle = (low+high)//2
+
+# 		if arr[middle] == elem:
+# 			return middle
+# 		elif arr[middle] > elem:
+# 			high = middle - 1
+# 		else:
+# 			low = middle + 1
+
+# 	return -1
+
+# print(bi_search([1,2,3,4,4,6,7,9],7))
+
+
+# class and static methods
+from datetime import date
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    @classmethod
+    def from_birth_year(cls,name,year):
+        return cls(name,date.today().year-year)
+
+    # for utility or helper functions, cannot instantiate object
+    @staticmethod
+    def isAdult(age):
+        return age > 18
+
+p1=Person("bhaarth",26)
+
+p2 = Person.from_birth_year("ram",1995)
+
+p3 = Person.isAdult(55)
+
+print(p1.age)
+
+print(p2.age)
+
+print(p3)
+
+print(Person.isAdult(22))
 
 
 
 
+# shallow copy
 
+import copy
 
+old_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+new_list = copy.copy(old_list)
 
+old_list[1][1] = 'AA'
 
+print("Old list:", old_list,id(old_list))
+print("New list:", new_list,id(new_list))
 
+'''
+o/p:-
+----
+Old list: [[1, 1, 1], [2, 'AA', 2], [3, 3, 3]]
+New list: [[1, 1, 1], [2, 'AA', 2], [3, 3, 3]]
 
+'''
 
+# deep copy
+
+import copy
+
+old_list = [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+new_list = copy.deepcopy(old_list)
+
+old_list[1][0] = 'BB'
+
+print("Old list:", old_list,id(old_list))
+print("New list:", new_list,id(new_list))
+
+# o/p
+''' 
+Old list: [[1, 1, 1], ['BB', 2, 2], [3, 3, 3]]
+New list: [[1, 1, 1], [2, 2, 2], [3, 3, 3]]
+'''
 
 
 
---
+++ test.py
@@ -0,0 +1,74 @@
+# nums = [1,2,2,2,3,3,4,5,5,5,7]
+
+# unique_elemts = []
+
+# def get_unique_elements(nums):
+# 	for i in nums:
+# 		if i not in unique_elemts:
+# 			unique_elemts.append(i)
+# 	return unique_elemts
+
+# output = get_unique_elements(nums)
+
+# print(output)
+
+
+def sum_two_numbers(nums, target):
+	dictionary = {}
+	array = []
+
+	for i in range(len(nums)):
+		secondNo = target-nums[i]
+		# print(secondNo,"secondNo")
+
+		if(secondNo in dictionary.keys()):
+			# secondIndex = nums.index(secondNo)
+			secondIndex = dictionary[secondNo]
+
+
+			# print(secondIndex,"secondIndex")
+
+			if(i != secondIndex):
+				# print(i,"i")
+				return sorted([i,secondIndex])
+		else:
+			dictionary[nums[i]] = i
+
+result = sum_two_numbers([1,2,3],3)
+# print(result)
+
+
+# def test_upload(client):
+#     data = {
+#         'Data': [20.0, 30.0, 401.0, 50.0],
+#         'Date': ['2017-08-11', '2017-08-12', '2017-08-13', '2017-08-14'],
+#         'Day': 4
+#     }
+#     url = '/upload/'
+
+#     response = client.post(url, json=data)
+
+#     assert response.content_type == 'application/json'
+#     assert response.json['Result'] == 39
+
+
+# decorator function to convert to lowercase
+def lowercase_decorator(function):
+   def wrapper():
+       func = function()
+       string_lowercase = func.lower()
+       return string_lowercase
+   return wrapper
+# decorator function to split words
+def splitter_decorator(function):
+   def wrapper():
+       func = function()
+       string_split = func.split()
+       return string_split
+   return wrapper
+
+@splitter_decorator # this is executed next
+@lowercase_decorator # this is executed first
+def hello():
+   return 'Hello World'
+print(hello()) 
